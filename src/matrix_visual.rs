d04617ad274faf91862c63afed3df2eaeb3893b0
dylanabzr <dylanab@pm.me>
2023-10-06 23:48:59 -0300

    removendo codigo comentado

--- src/matrix_visual.rs
+++ src/matrix_visual.rs
@@ -3,7 +3,6 @@ use rand::Rng;
 pub fn create_random_matrix(dest: &mut Vec<Vec<f32>>, size: usize){
     let mut rng = rand::thread_rng();
     *dest = vec![vec![0.0; size]; size];
-    //dest.resize(size, vec![0f32, size as f32]);
     for i in 0..size{
         for j in 0..size {dest[i][j] = rng.gen_range(1..10) as f32}
     }
