967937d0053582da25c48734bd296aee73e9ea4c
dylanabzr <dylanab@pm.me>
2023-10-07 00:04:25 -0300

    só facilitando uma parada

--- src/inverse_matrix.rs
+++ src/inverse_matrix.rs
@@ -5,8 +5,7 @@ use std::vec::Vec;
 pub fn inverse_matrix(matrix: Vec<Vec<f32>>, dest: &mut Vec<Vec<f32>>, size: usize) {
     let mut adjoint: Vec<Vec<f32>> = vec![vec![0.0; size]; size];
     *dest = vec![vec![0.0; size]; size];
-    let cloned_matrix = matrix.clone();
-    set_adjoint(cloned_matrix, &mut adjoint, size);
+    set_adjoint(matrix.clone(), &mut adjoint, size);
     let reverse_det: f32 = 1.0 / det(&matrix, size);
     for i in 0..size {
         for j in 0..size {
